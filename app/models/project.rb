3d16cb2317d648bc0e420bac30ba09cbee1ebfdb
Steve Faulkner <southpolesteve@gmail.com>
2012-08-25 10:53:03 -0500

    Handle date times better

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,12 +6,6 @@ class Project < ActiveRecord::Base
 
   def github_issues
     user.github.issues.list_repo owner, name
-
-    res.each_page do |page|
-      page.each do |repo|
-        puts repo.name
-      end
-    end
   end
 
   def import_issues
@@ -19,16 +13,17 @@ class Project < ActiveRecord::Base
     result.each do |github_issue|
       issues.create! do |issue|
         issue.body = github_issue["body"]
-        issue.closed_at = github_issue["closed_at"]
-        issue.github_created_at = github_issue["github_created_at"]
+        issue.closed_at = github_issue["closed_at"].try(:to_datetime)
+        issue.github_created_at = github_issue["created_at"].try(:to_datetime)
         issue.github_id = github_issue["github_id"]
-        issue.github_updated_at = github_issue["github_updated_at"]
+        issue.github_updated_at = github_issue["updated_at"].try(:to_datetime)
         issue.milestone = github_issue["milestone"]
         issue.number = github_issue["number"]
         issue.state = github_issue["state"]
         issue.title = github_issue["title"]
       end
     end
+    self
   end
 
 end
