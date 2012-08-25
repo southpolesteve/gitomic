9550b187370f0b59024ac2580fc155ef18e0921c
Steve Faulkner <southpolesteve@gmail.com>
2012-08-24 20:38:33 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Murfhub::Application.config.secret_token = 'b694a3d0d3d6f041e1712a9a19cd12987be7cad5b1b657e5a0b99b9a4035827a017af890d76e6000ac5c05c110b0c7e30594bb1559fdcdbcb69f74d694381b9c'
