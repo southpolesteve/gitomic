38c6d4b5b4f4f79be6ef29380cdbe43018bddb3a
Steve Faulkner <southpolesteve@gmail.com>
2012-08-25 11:44:22 -0500

    refactor of update from github method

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,21 +1,25 @@
 class Issue < ActiveRecord::Base
-  attr_accessible :body, :closed_at, :github_created_at, :github_id, :github_updated_at, :milestone, :number, :state, :title
+  attr_accessible :body, :closed_at, :github_created_at, 
+                  :github_id, :github_updated_at, :milestone, 
+                  :number, :state, :title
 
   belongs_to :project
 
   def update_from_github(user)
     issue = github_issue(user)
-    self.body = issue["body"]
-    self.closed_at = issue["closed_at"].try(:to_datetime)
-    self.github_created_at = issue["created_at"].try(:to_datetime)
-    self.github_id = issue["github_id"]
-    self.github_updated_at = issue["updated_at"].try(:to_datetime)
-    self.milestone = issue["milestone"]
-    self.number = issue["number"]
-    self.state = issue["state"]
-    self.title = issue["title"]
+    self.body = issue[:body]
+    self.closed_at = issue.closed_at.try(:to_datetime)
+    self.github_created_at = issue.created_at.try(:to_datetime)
+    self.github_id = issue.id
+    self.github_updated_at = issue.updated_at.try(:to_datetime)
+    self.milestone = issue.milestone
+    self.number = issue.number
+    self.state = issue.state
+    self.title = issue.title
     save!
   end
+
+  private
 
   def github_issue(user)
     issues = Github::Issues.new(oauth_token: user.github_token)
