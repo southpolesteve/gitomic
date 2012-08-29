24a7079551c37bcf7fab8d77e0788588827f2f37
Steve Faulkner <southpolesteve@gmail.com>
2012-08-29 11:00:45 -0500

    Add interface, and methods for interacting and importing issues

--- app/models/github/issue.rb
+++ app/models/github/issue.rb
@@ -1,9 +1,13 @@
 module Github
   class Issue
 
-    attr_accessor :body, :closed_at, :created_at, :id, :updated_at, :milestone, :number, :state, :title
+    attr_accessor :body, :closed_at, :created_at, :id, 
+    :updated_at, :milestone, :number, :state, :title,
+    :owner, :repo
 
-    def initialize(data)
+    def initialize(owner, repo, data)
+      @owner = owner
+      @repo = repo
       @body = data["body"]
       @closed_at = data["closed_at"].try(:to_datetime)
       @created_at = data["created_at"].try(:to_datetime)
@@ -13,6 +17,37 @@ module Github
       @number = data["number"]
       @state = data["state"]
       @title = data["title"]
+    end
+
+    def import
+      i = issue
+      attributes_map.except(:number).each do |key, value|
+        i.send("#{value}=", self.send(key))
+      end
+      i.save!
+      return i
+    end
+
+    def issue
+      project.issues.find_or_initialize_by_number(number)
+    end
+
+    def project
+      Project.find_by_owner_and_name owner,repo
+    end
+
+    private
+
+    def attributes_map
+      {:body => :body,
+      :closed_at => :closed_at,
+      :created_at => :github_created_at,
+      :id => :github_id,
+      :updated_at => :github_updated_at,
+      :number => :number,
+      :state => :github_state,
+      :title => :title,
+      :milestone => :milestone}
     end
 
   end
