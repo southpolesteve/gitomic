fca1d779e05abdfc3faa291ea8beb7aaba261435
Steve Faulkner <southpolesteve@gmail.com>
2012-08-29 11:05:04 -0500

    Remote old issue methods

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -20,36 +20,4 @@ class Issue < ActiveRecord::Base
 
   end
 
-  def github_issue(current_user)
-    issues = Github::Issues.new(oauth_token: current_user.github_token)
-    @response = issues.get project.owner, project.name, number
-  end
-
-  def create_github_issue(current_user)
-    issues = Github::Issues.new(oauth_token: current_user.github_token)
-    @response = issues.create project.owner, project.name, title: title, body: body, labels: [state]
-    update_from_github_response
-  end
-
-  def update_github_issue(current_user)
-    issues = Github::Issues.new(oauth_token: current_user.github_token)
-    @response = issues.edit project.owner, project.name, number, title: title, body: body, labels: [state]
-    update_from_github_response
-  end
-
-  private
-
-  def update_from_github_response
-    self.body = @response[:body]
-    self.closed_at = @response.closed_at.try(:to_datetime)
-    self.github_created_at = @response.created_at.try(:to_datetime)
-    self.github_id = @response.id
-    self.github_updated_at = @response.updated_at.try(:to_datetime)
-    self.milestone = @response.milestone
-    self.number = @response.number
-    self.github_state = @response.state
-    self.title = @response.title
-    save!
-  end
-
 end
