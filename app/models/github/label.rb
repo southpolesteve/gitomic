e781d884bcb54c4f5fce8025bf96267f44894395
Steve Faulkner <southpolesteve@gmail.com>
2012-08-29 14:26:12 -0500

    Put back in parens around method params

--- app/models/github/label.rb
+++ app/models/github/label.rb
@@ -3,7 +3,7 @@ module Github
 
     attr_accessor :color, :name, :owner, :repo
 
-    def initialize owner,repo,data 
+    def initialize(owner, repo, data)
       @owner = owner
       @repo = repo
       @color = data["color"]
