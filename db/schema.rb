573b15f1ef6f5140ba58b2a1757fb242b7ec90ba
Steve Faulkner <southpolesteve@gmail.com>
2012-09-03 14:57:14 -0500

    Add imported_at to project, not user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901035452) do
+ActiveRecord::Schema.define(:version => 20120903194819) do
 
   create_table "issue_labels", :force => true do |t|
     t.integer  "label_id"
@@ -57,9 +57,10 @@ ActiveRecord::Schema.define(:version => 20120901035452) do
   create_table "projects", :force => true do |t|
     t.string   "owner"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.datetime "imported_at"
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
