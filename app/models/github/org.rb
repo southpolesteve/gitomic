a1eac1fe52564a7edc91a671ca1a2a9a5b6f28c9
Steve Faulkner <southpolesteve@gmail.com>
2012-09-05 14:35:33 -0500

    Add method for looking at repos of user

--- app/models/github/org.rb
+++ app/models/github/org.rb
@@ -1,12 +1,17 @@
 module Github
   class Org
 
-    attr_accessor
+    attr_accessor :name, :avatar, :id
 
     def initialize(data)
       @name = data['login']
       @avatar = data['avatar_url']
       @id = data['id']
+    end
+
+    def repos(user)
+      github = Github::API.new(user)
+      github.org_repos name
     end
 
   end
