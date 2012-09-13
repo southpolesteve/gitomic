c2770e432ff0d0298ad56f2f743f806782ee91ed
Steve Faulkner <southpolesteve@gmail.com>
2012-09-12 23:20:58 -0500

    A start on selecting text color for label

--- app/models/label.rb
+++ app/models/label.rb
@@ -8,4 +8,14 @@ class Label < ActiveRecord::Base
 
   scope :without_list, where(:list_id => nil)
 
+  def text_color
+    rgb = color.scan(/../).map {|color| color.to_i(16)}
+    brightness = ((rgb[0]*299) + (rgb[1]*587) + (rgb[2]*114)) / 1000
+    if brightness < 186
+      "ffffff"
+    else
+      "303030"
+    end
+  end
+
 end
