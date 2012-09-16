5ce4bb4235806cf1113d2c130378db0b2e23414f
Steve Faulkner <southpolesteve@gmail.com>
2012-09-16 17:42:43 -0500

    Add bare bones page for production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     if current_user
       redirect_to projects_path and return
     end
+
+    if Rails.env != "production"
+      render "landing", :layout => "landing"
+    end
+
   end
 end
