9af058bb3e346312347687d6d0d45fb62456b111
Steve Faulkner <southpolesteve@gmail.com>
2012-10-08 23:02:20 -0500

    Add owner and repo name to github issue

--- app/models/github/issue.rb
+++ app/models/github/issue.rb
@@ -3,13 +3,15 @@ module Github
 
     attr_accessor :body, :closed_at, :created_at, :id, 
     :updated_at, :milestone, :number, :state, :title,
-    :labels, :url, :assignee
+    :labels, :url, :assignee, :owner, :repo_name
 
-    def initialize(data)
+    def initialize(owner, repo_name, data)
       [:body, :id, :number, :state, :title].each do |key|
         self.send("#{key}=", data.send(key))
       end
 
+      self.owner = owner
+      self.repo_name = repo_name
       self.created_at = Time.parse(data.created_at) if data.created_at
       self.updated_at = Time.parse(data.updated_at) if data.updated_at
       self.closed_at = Time.parse(data.closed_at) if data.closed_at
@@ -20,10 +22,39 @@ module Github
       issues = []
       github = Github::Issues.new oauth_token: user.github_token
       github.list_repo(owner, repo_name, opts).each do |issue|
-        issues << Github::Issue.new(issue)
+        issues << Github::Issue.new(owner, repo_name, issue)
       end
       issues
     end
+
+    # def import
+    #   i = issue
+    #   attributes_map.except(:number).each do |key, value|
+    #     i.send("#{value}=", self.send(key))
+    #   end
+    #   labels.each do |label|
+    #     found_label = project.labels.find_by_name(label.name)
+    #     i.labels << found_label unless i.labels.include?(found_label)
+    #   end
+    #   i.save!
+
+    #   return i
+    # end
+
+    # private
+
+    # def attributes_map
+    #   {:body => :body,
+    #   :closed_at => :closed_at,
+    #   :created_at => :github_created_at,
+    #   :id => :github_id,
+    #   :updated_at => :github_updated_at,
+    #   :number => :number,
+    #   :state => :github_state,
+    #   :title => :title,
+    #   :milestone => :milestone,
+    #   :url => :github_url}
+    # end
 
   end
 end
