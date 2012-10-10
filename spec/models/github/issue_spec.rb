a9ea8f4e20fb426f0d6c87c57c81bf820257864c
Steve Faulkner <southpolesteve@gmail.com>
2012-10-10 00:08:11 -0500

    Add pending issue specs

---
+++ spec/models/github/issue_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Github::Issue do
+  let(:user) { FactoryGirl.build :test_user }
+
+  describe '.new' do
+    it "is pending"
+  end
+
+  describe '.list_repo' do
+    it "is pending"
+  end
+
+  describe '.gitomic_issue' do
+    it "is pending"
+  end
+
+  describe '.gitomic_project' do
+    it "is pending"
+  end
+
+end
