9dd102aa4ca9a5f8e67ca372f620bee11db6ab63
Steve Faulkner <southpolesteve@gmail.com>
2012-10-10 23:33:56 -0500

    Add pending project specs

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Project do
+
+  describe '.import_labels' do
+    it "pending"
+  end
+
+  describe '.import_team' do
+    context 'for a user repo' do
+      it "pending"
+    end
+
+    context 'for a org repo' do
+      it "pending"
+    end
+  end
+
+  describe '.import_issues' do
+    it "pending"
+  end
+
+end
