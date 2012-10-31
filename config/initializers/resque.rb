0bbab060d0e901cea751806b113e61f2548a05d9
Steve Faulkner <southpolesteve@gmail.com>
2012-10-30 22:45:17 -0500

    Setup resque initializer for production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,11 @@
 require 'resque/server'
 
+ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
+uri = URI.parse(ENV["REDISTOGO_URL"])
+redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+Resque.inline = true if Rails.env == 'test'
+Resque.redis = redis
 
 #hack for having resque reload classes in development
 unless Rails.application.config.cache_classes
