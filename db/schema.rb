39c77070a63b620851a969b56d1b31bec38d07b5
Steve Faulkner <southpolesteve@gmail.com>
2012-10-30 20:24:17 -0500

    Run migrations. Delete old files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925032230) do
+ActiveRecord::Schema.define(:version => 20121031010915) do
 
   create_table "issue_labels", :force => true do |t|
     t.integer  "label_id"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(:version => 20120925032230) do
     t.string   "avatar"
     t.string   "github_token"
     t.string   "github_login"
+    t.boolean  "admin"
   end
 
 end
