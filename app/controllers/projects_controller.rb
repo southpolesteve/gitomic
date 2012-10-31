c135cf34c52e0a55cc32eda565d9ac88ac476930
Steve Faulkner <southpolesteve@gmail.com>
2012-10-30 22:08:35 -0500

    Add twitter modal for creating new issue. All over the place tonight

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = current_user.projects.find(params[:id])
+    @new_issue = @project.issues.new
   end
 
   def settings
