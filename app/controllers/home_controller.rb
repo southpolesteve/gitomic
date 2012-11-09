0b7ea92f8d2c20b54e830dad7ac042da9e369c7f
Steve Faulkner <southpolesteve@gmail.com>
2012-11-08 23:15:42 -0600

    Remove call to layout landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,5 @@ class HomeController < ApplicationController
     if current_user
       redirect_to projects_path and return
     end
-
-    if Rails.env == "production"
-      render "landing", :layout => "landing"
-    end
-
   end
 end
