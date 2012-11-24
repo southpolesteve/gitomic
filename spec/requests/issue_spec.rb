72d20a4cfc3a53c95c804f33bcb55d25baf589c9
Steve Faulkner <southpolesteve@gmail.com>
2012-11-24 15:15:26 -0600

    Passing integration test for creating an issue

--- spec/requests/issue_spec.rb
+++ spec/requests/issue_spec.rb
@@ -20,3 +20,24 @@ describe "updating an issue" do
     original_priority.should_not eq(issue_2.reload.priority)
   end
 end
+
+describe "creating an issue" do
+  let(:user){ FactoryGirl.create :test_user }
+  let(:project){ FactoryGirl.create :imported_project, creator: user }
+
+  before do
+    Github::Issue.stub create: FactoryGirl.build(:github_issue)
+  end
+
+  it 'creates the issue', :js do
+    login user
+    visit project_path(project)
+    click_on "New Issue"
+    sleep(5) #HACK find a better way
+    within '.modal' do
+      fill_in "Title", :with => 'A test issue'
+      click_on "Create Issue"
+    end
+    page.should have_content('A test issue')
+  end
+end
